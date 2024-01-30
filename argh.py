bff895e9fc8f63022d20778b33c9f126f3234114
clarabardeen <157635285+clarabardeen@users.noreply.github.com>
2024-01-29 22:57:54 -0800

    Update argh.py

--- argh.py
+++ argh.py
@@ -1,1 +1,2 @@
-print(23465+6712+(6789265*218)+45) #this represents how limited my knowledge is in coding
+print(23465+6712+(6789265*218)+45) #this represents how limited my knowledge is in coding I also chose this file name as pirates are a little more scary than dragons. 
+# to run this code I would go to terminal and put python (filename).py so in this case it would be python argh.py
