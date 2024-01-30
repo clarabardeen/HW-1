c4a4ce629087826a47e8dd5a18dddf03b3ac7c5e
clarabardeen <157635285+clarabardeen@users.noreply.github.com>
2024-01-30 14:32:10 -0800

    Update argh.py

--- argh.py
+++ argh.py
@@ -1,3 +1,4 @@
 print(23465+6712+(6789265*218)+45) #this represents how limited my knowledge is in coding
 # I also chose this file name as pirates are a little more scary than dragons. 
-# To run this code I would go to terminal and put python (filename).py so in this case it would be python argh.py
+# To run this code I would go to terminal and enter this directory so cd HW-1 
+# then I would put python (filename).py so in this case it would be python argh.py
